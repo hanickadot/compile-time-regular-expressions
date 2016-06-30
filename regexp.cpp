c89dfde9b36013d86f1ff12ccf05d62e1784f37c
Hana Dusíková <hana.dusikova@avg.com>
2016-06-30 12:43:50 +0200

    Renaming.

--- regexp.cpp
+++ regexp.cpp
@@ -2,7 +2,7 @@
 
 #include <iostream>
 
-SYMBOL(RegExp);
+SYMBOL(S);
 SYMBOL(Part);
 SYMBOL(Symbol);
 
@@ -11,27 +11,33 @@ SYMBOL(Char_Begin);
 SYMBOL(Char_End);
 SYMBOL(Char_ABC);
 
-template <char c> struct Char {
-};
+namespace RegExp {
+	
+	template <typename ...> struct RegExp {
+	};
 
-template <typename ...> struct RegExpBase {
-};
+	template <char c> struct Char {
+	};
 
-struct Anything {
+	struct Anything {
 
-};
+	};
 
-struct Begin {
+	struct Begin {
 
-};
+	};
 
-struct End {
+	struct End {
 
-};
+	};
+	
+}
+
+
 
 template <typename What> struct Add {
-	template <char c, typename ... Definition, typename ... Content> static constexpr auto build(const Static::Stack<RegExpBase<Definition...>, Content...> &&) {
-		return Static::Stack<RegExpBase<Definition..., What>,Content...>{};
+	template <char c, typename ... Definition, typename ... Content> static constexpr auto build(const Static::Stack<RegExp::RegExp<Definition...>, Content...> &&) {
+		return Static::Stack<RegExp::RegExp<Definition..., What>,Content...>{};
 	}
 	template <char c, typename ... Content> static constexpr auto build(const Static::Stack<Content...> &&) {
 		return Static::Stack<Content...,What>{};
@@ -39,24 +45,24 @@ template <typename What> struct Add {
 };
 
 template <template <char c> typename What> struct AddChar {
-	template <char c, typename ... Definition, typename ... Content> static constexpr auto build(const Static::Stack<RegExpBase<Definition...>, Content...> &&) {
-		return Static::Stack<RegExpBase<Definition..., What<c>>,Content...>{};
+	template <char c, typename ... Definition, typename ... Content> static constexpr auto build(const Static::Stack<RegExp::RegExp<Definition...>, Content...> &&) {
+		return Static::Stack<RegExp::RegExp<Definition..., What<c>>,Content...>{};
 	}
 	template <char c, typename ... Content> static constexpr auto build(const Static::Stack<Content...> &&) {
 		return Static::Stack<Content...,What<c>>{};
 	}
 };
 
-template <char ... c> struct Static::Table<RegExp, c...> { using Move = Part; };
+template <char ... c> struct Static::Table<S, c...> { using Move = Part; };
 
 template <char c> struct Static::Table<Part, c> { using Move = Static::String<Symbol, Part>; };
 template <> struct Static::Table<Part> { using Move = Epsilon; };
 
-template <> struct Static::Table<Symbol,'.'> { using Move = Static::String<Builder<Add<Anything>>, Char_Anything>; };
-template <> struct Static::Table<Symbol,'^'> { using Move = Static::String<Builder<Add<Begin>>, Char_Begin>; };
-template <> struct Static::Table<Symbol,'$'> { using Move = Static::String<Builder<Add<End>>, Char_End>; };
+template <> struct Static::Table<Symbol,'.'> { using Move = Static::String<Builder<Add<RegExp::Anything>>, Char_Anything>; };
+template <> struct Static::Table<Symbol,'^'> { using Move = Static::String<Builder<Add<RegExp::Begin>>, Char_Begin>; };
+template <> struct Static::Table<Symbol,'$'> { using Move = Static::String<Builder<Add<RegExp::End>>, Char_End>; };
 
-template <char c> struct Static::Table<Symbol,c> { using Move = Static::String<Builder<AddChar<Char>>, Char_ABC>; };
+template <char c> struct Static::Table<Symbol,c> { using Move = Static::String<Builder<AddChar<RegExp::Char>>, Char_ABC>; };
 
 template <> struct Static::Table<Char_Anything,'.'> { using Move = ReadChar; };
 template <> struct Static::Table<Char_Begin,'^'> { using Move = ReadChar; };
@@ -70,15 +76,18 @@ template <> struct Static::Table<Char_ABC,'$'> { using Move = RejectInput; };
 
 template<typename CharT, CharT ... string> constexpr auto operator""_re() {
 	using namespace Static;
-	using Parser = Decider<Stack<RegExp>, Input<string...>>;
+	using namespace RegExp;
+	
+	using Parser = Decider<Stack<S>, Input<string...>>;
 	static_assert(Parser::correct);
-	return typename Parser::template Type<RegExpBase<>>{};
+	return typename Parser::template Type<RegExp<>>{};
 }
 
 int main() {
-	static_assert( std::is_same<decltype("x$"_re), RegExpBase<Char<'x'>,End>> ::value);
-	static_assert( std::is_same<decltype("^x$"_re), RegExpBase<Begin,Char<'x'>,End>> ::value);
-	static_assert( std::is_same<decltype("^xy$"_re), RegExpBase<Begin,Char<'x'>,Char<'y'>,End>> ::value);
-	static_assert( std::is_same<decltype("^."_re), RegExpBase<Begin,Anything>> ::value);
+	using namespace RegExp;
+	static_assert( std::is_same<decltype("x$"_re), RegExp<Char<'x'>,End>> ::value);
+	static_assert( std::is_same<decltype("^x$"_re), RegExp<Begin,Char<'x'>,End>> ::value);
+	static_assert( std::is_same<decltype("^xy$"_re), RegExp<Begin,Char<'x'>,Char<'y'>,End>> ::value);
+	static_assert( std::is_same<decltype("^."_re), RegExp<Begin,Anything>> ::value);
 
 }
