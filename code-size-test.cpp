09329226b926e7e0606546aebf3a21feb508fb8d
Hana Dusíková <hana.dusikova@avg.com>
2016-07-02 21:56:05 +0200

    static_assert instead

--- code-size-test.cpp
+++ code-size-test.cpp
@@ -1,9 +1,14 @@
 #include "pregexp.hpp"
 
 int main(int argc, char ** argv) {
-	
 	using namespace sre;
-	checkRegExp<Begin,Select<Char<'a','b','c'>,String<'x','y','z'>>,Plus<Anything>,End>("^([abc]|xyz).+$"_pre);
+	
+	static_assert(
+		std::is_same<
+			RegExp<Begin,Select<Char<'a','b','c'>,String<'x','y','z'>>,Plus<Anything>,End>,
+			decltype("^([abc]|xyz).+$"_pre)
+		>::value
+	);
 	
 	if (argc >= 2 && "^([abc]|xyz).+$"_pre.match(argv[1])) {
 		puts("match");
