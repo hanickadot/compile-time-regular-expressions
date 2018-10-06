9f8e80286d50560950c76d1a7de6c6ed697c3a73
Uilian Ries <uilianries@gmail.com>
2018-10-06 11:54:22 -0300

    Remove SCM from Conan
    - SCM is remote project feature. When using it Conan
      will clone (again) all project from remote URL.
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -9,16 +9,9 @@ class CtreConan(ConanFile):
     author = "Hana Dusíková (ctre@hanicka.net)"
     description = "Compile Time Regular Expression for C++17/20"
     homepage = "https://github.com/hanickadot/compile-time-regular-expressions"
-    name = "CTRE"
-    version = "2.0"
     exports = "LICENSE"
     exports_sources = "include/*"
     no_copy_source = True
-    scm = {
-        "type": "git",
-        "url": url,
-        "revision": "master"
-    }
     
     def package(self):
         self.copy("*.hpp")
