65dcda473074b50e1276a0457f67f22bea8b8cc2
Uilian Ries <uilianries@gmail.com>
2018-10-08 11:04:30 -0300

    #1 Use dynamic project version from Travis branch
    - Replace hardcoded version in Conan recipe by
      branch name on Travis CI envs
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- .conan/build.py
+++ .conan/build.py
@@ -4,9 +4,18 @@ import os
 from cpt.packager import ConanMultiPackager
 
 
+def get_travis_branch():
+    return os.getenv("TRAVIS_BRANCH")
+
+
+def get_reference():
+    return "CTRE/{}".format(get_travis_branch())
+
+
 if __name__ == "__main__":
     builder = ConanMultiPackager(
-        username="crte",
+        reference=get_reference(),
+        username="ctre",
         upload="https://api.bintray.com/conan/hanickadot/ctre",
         upload_only_when_stable=True,
         stable_branch_pattern="v?\d+\.\d+.*",
