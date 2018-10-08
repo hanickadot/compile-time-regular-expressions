97149009ff8ce88b5cdfa1b3ba2acc526bc3eeb1
Hana Dusíková <hana.dusikova@avast.com>
2018-10-08 15:39:45 +0200

    upgrade version

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ from conans import ConanFile
 
 class CtreConan(ConanFile):
     name = "CTRE"
-    version = "2.0"
+    version = "2.1"
     license = "MIT"
     url = "https://github.com/hanickadot/compile-time-regular-expressions"
     author = "Hana Dusíková (ctre@hanicka.net)"
