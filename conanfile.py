95d558d36124951969298b574645c3ef27921e78
Hana Dusíková <hana.dusikova@avast.com>
2018-10-14 18:50:47 +0200

    remove non-ascii chars from conan-file

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ class CtreConan(ConanFile):
     name = "CTRE"
     license = "MIT"
     url = "https://github.com/hanickadot/compile-time-regular-expressions"
-    author = "Hana Dusíková (ctre@hanicka.net)"
+    author = "Hana Dusikova (ctre@hanicka.net)"
     description = "Compile Time Regular Expression for C++17/20"
     homepage = "https://github.com/hanickadot/compile-time-regular-expressions"
     no_copy_source = True
