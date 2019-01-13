7de332502fd1f6986b45ed3bb163f2d583f1cdce
Hana Dusíková <hana.dusikova@avast.com>
2019-01-13 20:36:48 +0100

    test for CNTTP

--- tests/many-of-same-proto.cpp
+++ tests/many-of-same-proto.cpp
@@ -1,7 +1,7 @@
 #include <ctre.hpp>
 using namespace ctre::test_literals;
 
-#if not(__GNUC__ == 9 && __GNUC_MINOR__ == 0 && __GNUC_PATCHLEVEL__ == 0)
+#if !__cpp_nontype_template_parameter_class
 #define CTRE_TEST(pattern) static_assert(pattern ## _ctre_test)
 #else
 
--- tests/parsing.cpp
+++ tests/parsing.cpp
@@ -2,7 +2,7 @@
 
 using namespace ctre::test_literals;
 
-#if not(__GNUC__ == 9 && __GNUC_MINOR__ == 0 && __GNUC_PATCHLEVEL__ == 0)
+#if !__cpp_nontype_template_parameter_class
 #define CTRE_TEST(pattern) (pattern ## _ctre_test)
 #else
 
