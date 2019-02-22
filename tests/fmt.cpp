cf4eff6ae623c0ae61ab6779f65f14542d50c28a
Hana Dusíková <hana.dusikova@avast.com>
2019-02-22 11:04:32 -1000

    example of fmt

--- tests/fmt.cpp
+++ tests/fmt.cpp
@@ -1,18 +1,13 @@
 #include <ctfmt.hpp>
 #include <iostream>
 
-
 static constexpr ctll::basic_fixed_string pattern = "Hello {1} from {0}!\n";
 
-template <typename T> struct identify;
 
-int main() {
+int main(int argc, char ** argv) {
 	auto fmt = ctfmt::format<pattern>();
-	//identify<decltype(fmt)> hello;
-	std::array<char, 64> buffer;
 	
-	std::string_view a = fmt(buffer, "Hana","std::format");
+	
+	std::string a = fmt("Hana", "std::format");
 	fwrite(a.data(), a.size(), 1, stdout);
-	std::string_view b = fmt(buffer, "C++20","programmers");
-	fwrite(b.data(), b.size(), 1, stdout);
 }
