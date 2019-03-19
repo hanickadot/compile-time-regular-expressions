9d0599dcbe370003c5832fc9b04cd58f7f09a42d
Hana Dusíková <hana.dusikova@avast.com>
2019-03-19 19:30:09 +0100

    remove asserts

--- include/unicode-db/unicode.hpp
+++ include/unicode-db/unicode.hpp
@@ -14317,15 +14317,3 @@ constexpr numeric_value cp_numeric_value(char32_t cp) {
 
 }    // namespace uni
 
-
-static_assert(uni::cp_script('C') == uni::script::latin);
-static_assert(uni::cp_block(U'🎉') == uni::block::misc_pictographs);
-static_assert(!uni::cp_is<uni::property::xid_start>('1'));
-static_assert(uni::cp_is<uni::property::xid_continue>('1'));
-static_assert(uni::cp_age(U'🤩') == uni::version::v10_0);
-static_assert(uni::cp_is<uni::property::alphabetic>(U'ß'));
-static_assert(uni::cp_category(U'🦝') == uni::category::so);
-static_assert(uni::cp_is<uni::category::lowercase_letter>('a'));
-static_assert(uni::cp_is<uni::category::letter>('a'));
-
-static_assert(uni::__get_binary_prop<uni::__binary_prop_from_string("Emoji")>(U'🤩'));
