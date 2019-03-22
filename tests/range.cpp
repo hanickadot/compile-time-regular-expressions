e34fba9cc64b506b493ba632601e9da04d0e592b
Hana Dusíková <hana.dusikova@avast.com>
2019-03-22 21:31:56 +0100

    gcc 8 test fix

--- tests/range.cpp
+++ tests/range.cpp
@@ -1,6 +1,8 @@
 #include <ctre.hpp>
 #include <iostream>
 
+static constexpr auto pattern = ctll::fixed_string("(?<first>[0-9])[0-9]++");
+
 int main() {
 	using namespace std::string_view_literals;
 	auto input = "123,456,768"sv;
@@ -9,7 +11,7 @@ int main() {
 	    for (auto match: ctre::range<"(?<first>[0-9])[0-9]++">(input)) {
 	#else
 		using namespace ctre::literals;
-		static constexpr auto pattern = ctll::fixed_string("(?<first>[0-9])[0-9]++");
+		
 		for (auto match: ctre::range<pattern>(input)) {
 	#endif
 			
