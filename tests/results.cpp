b9cb488116efbd26f8edadfec18afd2f265b0ded
Hana Dusíková <hana.dusikova@avast.com>
2020-07-21 14:18:35 +0200

    char_conv is not in xcode

--- tests/results.cpp
+++ tests/results.cpp
@@ -24,12 +24,17 @@ int main() {
 		if ("456" == capture) std::cout << "bingo: ";
 		if ("768" != capture) std::cout << "bad: ";
 		
-		int value = 0;
-		auto [ptr, err] = std::from_chars(std::data(match), std::data(match)+std::size(match), value);
-		if (err == std::errc{}) {
-			std::cout << value << "\n";
-		} else {
-			std::cout << "error\n";
-		}
+		[[maybe_unused]] const char * ptr = std::data(match);
+		[[maybe_unused]] size_t length = std::size(match);
+		
+		[[maybe_unused]] const char * ptr2 = std::data(capture);
+		[[maybe_unused]] size_t length2 = std::size(capture);
+		
+		//auto [ptr, err] = std::from_chars(std::data(match), std::data(match)+std::size(match), value);
+		//if (err == std::errc{}) {
+		//	std::cout << value << "\n";
+		//} else {
+		//	std::cout << "error\n";
+		//}
 	}
 }
