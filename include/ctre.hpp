2b616243fb921155ccf06e436c8348bf8237005e
José Manuel Ramos Ruiz <46030516+jmramosr@users.noreply.github.com>
2020-09-16 13:29:40 +0200

    Redundant include guards
    To reduce compilation time explodes in ctre.hpp

--- include/ctre.hpp
+++ include/ctre.hpp
@@ -1,10 +1,24 @@
 #ifndef CTRE_V2__CTRE__HPP
 #define CTRE_V2__CTRE__HPP
 
+#ifndef CTRE_V2__CTRE__LITERALS__HPP
 #include "ctre/literals.hpp"
+#endif
+
+#ifndef CTRE_V2__CTRE__FUNCTIONS__HPP
 #include "ctre/functions.hpp"
+#endif
+
+#ifndef CTRE_V2__CTRE__ITERATOR__HPP
 #include "ctre/iterators.hpp"
+#endif
+
+#ifndef CTRE_V2__CTRE__RANGE__HPP
 #include "ctre/range.hpp"
+#endif
+
+#ifndef CTRE_V2__CTRE__OPERATORS__HPP
 #include "ctre/operators.hpp"
+#endif
 
 #endif
