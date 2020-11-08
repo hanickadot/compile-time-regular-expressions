e492417c90fc18a9bb2cf3d3eb3252a23f57c433
Hana Dusíková <hana.dusikova@avast.com>
2020-11-08 19:43:49 +0100

    problem with cycle found <- test here

---
+++ tests/_bad_cycle.cpp
@@ -0,0 +1,19 @@
+#include <ctre.hpp>
+#include <string_view>
+
+template <typename Subject, typename Pattern> constexpr bool match(Subject input, Pattern) {
+	return ctre::regular_expression<Pattern>::match(input);
+}
+
+template <typename Subject, typename Pattern> constexpr bool search(Subject input, Pattern) {
+	return ctre::regular_expression<Pattern>::search(input);
+}
+
+template <typename Subject, typename Pattern> constexpr bool starts_with(Subject input, Pattern) {
+	return ctre::regular_expression<Pattern>::starts_with(input);
+}
+
+using namespace std::string_view_literals;
+
+static_assert(match("ab"sv, ctre::plus<ctre::select<ctre::string<'a','b'>, ctre::character<'a'>>>()), "(ab|a)+");
+static_assert(match("ab"sv, ctre::plus<ctre::select<ctre::character<'a'>, ctre::string<'a','b'>>>()), "(a|ab)+");
