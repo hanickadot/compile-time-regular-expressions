d54da2efd60024d7291a7a281f58a088efa8f19e
Hana Dusíková <hana.dusikova@avast.com>
2020-11-15 11:48:12 +0100

    bitmask for flags

--- include/ctre/flags_and_modes.hpp
+++ include/ctre/flags_and_modes.hpp
@@ -7,28 +7,32 @@ struct singleline { };
 struct multiline { };
 
 struct flags {
-	bool block_empty_match = false;
-	bool multiline = false;
+	enum values: uint8_t {
+		block_empty_match = 1u,
+		multiline = 1u << 1u,
+	};
+	
+	uint8_t value = 0;
 	constexpr CTRE_FORCE_INLINE flags(ctre::singleline) { }
-	constexpr CTRE_FORCE_INLINE flags(ctre::multiline): multiline{true} { }
+	constexpr CTRE_FORCE_INLINE flags(ctre::multiline): value{values::multiline} { }
 };
 
 constexpr CTRE_FORCE_INLINE auto not_empty_match(flags f) {
-	f.block_empty_match = true;
+	f.value |= flags::block_empty_match;
 	return f;
 }
 
 constexpr CTRE_FORCE_INLINE auto consumed_something(flags f, bool condition = true) {
-	if (condition) f.block_empty_match = false;
+	if (condition) f.value &= ~flags::block_empty_match;
 	return f;
 }
 
 constexpr CTRE_FORCE_INLINE bool cannot_be_empty_match(flags f) {
-	return f.block_empty_match;
+	return f.value & flags::block_empty_match;
 }
 
 constexpr CTRE_FORCE_INLINE bool multiline_mode(flags f) {
-	return f.multiline;
+	return f.value & flags::multiline;
 }
 
 } // namespace ctre
