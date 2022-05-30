ef09e83624b705d30ea2589ef65533d6ed9b8175
Hana Dusíková <hana.dusikova@avast.com>
2022-05-30 15:00:05 +0200

    fix tests

--- tests/ci.cpp
+++ tests/ci.cpp
@@ -1,7 +1,21 @@
 #include <ctre.hpp>
 
-static_assert(ctre::match<"aloha">("aloha"));
-static_assert(!ctre::match<"aloha">("ALOHA"));
+#if CTRE_CNTTP_COMPILER_CHECK
 
-static_assert(ctre::match<"aloha", ctre::case_insensitive>("aloha"));
-static_assert(ctre::match<"aloha", ctre::case_insensitive>("ALOHA"));
+#define TEST_MATCH(pattern, subject) ctre::match<pattern>(subject)
+
+#define TEST_CI_MATCH(pattern, subject) ctre::match<pattern, ctre::case_insensitive>(subject)
+
+#else
+
+#define TEST_MATCH(pattern, subject) []{ static constexpr inline auto _ptn ## __LINE__ = ctll::fixed_string(pattern); return ctre::re<_ptn ## i__LINE__d, ctre::match_method, ctre::case_insensitive>(subject); }()
+
+#define TEST_CI_MATCH(pattern, subject) []{ static constexpr inline auto _ptn ## __LINE__ = ctll::fixed_string(pattern); return ctre::re<_ptn ## i__LINE__d, ctre::match_method, ctre::case_insensitive>(subject); }()
+
+#endif
+
+static_assert(TEST_MATCH("aloha","aloha"));
+static_assert(!TEST_MATCH("aloha","ALOHA"));
+
+static_assert(TEST_CI_MATCH("aloha", "aloha"));
+static_assert(TEST_CI_MATCH("aloha", "ALOHA"));
