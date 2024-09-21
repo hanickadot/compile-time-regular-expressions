626313dd1563e6f3a02e35427052d808df636cd8
Hana Dusíková <hanicka@hanicka.net>
2024-09-21 20:34:52 +0200

    fix for old compilers

--- tests/string.cpp
+++ tests/string.cpp
@@ -1,25 +1,16 @@
 #include <ctll/fixed_string.hpp>
 
-template <ctll::fixed_string Str> struct helper {
-	consteval bool correct() const noexcept {
-		return Str.correct();
-	}
-	consteval size_t size() const noexcept {
-		return Str.size();
-	}
-};
-
-constexpr auto a = helper<"\0\0">{};
+constexpr auto a = ctll::fixed_string{"\0\0"};
 
 static_assert(a.correct());
 static_assert(a.size() == 2u);
 
-constexpr auto b = helper<"\0">{};
+constexpr auto b = ctll::fixed_string{"\0"};
 
 static_assert(b.correct());
 static_assert(b.size() == 1u);
 
-constexpr auto c = helper<"">{};
+constexpr auto c = ctll::fixed_string{""};
 
 static_assert(c.correct());
 static_assert(c.size() == 0u);
